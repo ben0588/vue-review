99e5cb646df4db619b12a2e3d426fa035fc900fc
ben0588 <energy95272z@gmail.com>
2024-01-04 11:38:06 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/vite-vue-deploy-1/',
+  base: '/vue-review/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
